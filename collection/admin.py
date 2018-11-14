3f98f0b9853f924a515b49f297bf2af6e65217cb
tiana-horn <tiana.horn@gmail.com>
2018-11-14 15:08:14 -0500

    Admin Page

---
+++ collection/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from collection.models import Product
+# set up automated slug creation
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+# and register it
+admin.site.register(Product, ProductAdmin)
