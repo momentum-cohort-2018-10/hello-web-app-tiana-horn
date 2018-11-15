27caabd2a72adf38807b75c9e5b20aa7434cd35f
tiana-horn <tiana.horn@gmail.com>
2018-11-15 15:29:41 -0500

    Product Form

---
+++ collection/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from collection.models import Product
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ('name', 'description',)
+        
