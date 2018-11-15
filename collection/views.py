72b8d4cd9827658c726f547489e0b4069500d809
tiana-horn <tiana.horn@gmail.com>
2018-11-15 15:29:02 -0500

    Updated Views

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from collection.forms import ProductForm
 from collection.models import Product
 
 # Create your views here.
@@ -14,3 +15,17 @@ def product_detail(request, slug):
         'product' : product,
     })
 
+def edit_product(request, slug):
+    product = Product.objects.get(slug=slug)
+    form_class = ProductForm
+    if request.method == 'POST':
+        form = form_class(data=request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_detail', slug=product.slug)
+    else:
+        form = form_class(instance=product)
+    return render(request, 'products/edit_product.html', {
+        'product': product,
+        'form': form,
+    })
