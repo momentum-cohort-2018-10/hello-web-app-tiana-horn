6a0f32bd00863ea51b686a50463ae7740e1bae79
tiana-horn <tiana.horn@gmail.com>
2018-11-15 23:50:16 -0500

    Comb added

--- collection/products.py
+++ collection/products.py
@@ -7,7 +7,7 @@ products = [
         "description":
         "Moisturize without compromise! Our combination of natural, simple and pure ingredients will increase the health of your hair without adding any harsh chemicals. Lasts 3-6 months! Ingredients: Shea Butter, Coconut Oil, Cocoa Butter, Grapeseed Oil, Extra Virgin Olive Oil, Sunflower Oil, Rosemary Oil *This product was formerly known as Cocoa and Shea Butter Leave-In Moisturizer. New name, same great product!",
         "price":
-        $14.99,
+        "$14.99",
     },
     {
         "imgsrc":
@@ -17,6 +17,16 @@ products = [
         "description":
         "Our natural bamboo wooden combs will detangle your hair and now have twice the strength of our wooden hair picks! Great for coarse or thick hair.",
         "price":
-        $5.97,
+        "$5.97",
+    },
+    {
+        "imgsrc":
+        "Comb.jpg",
+        "name":
+        "Wooden Flower Child Remedies Hair Comb",
+        "description":
+        "Our natural bamboo wooden combs will detangle your hair and now have twice the strength of our wooden hair picks! Great for coarse or thick hair.",
+        "price":
+        "$8.50",
     },
 ]
