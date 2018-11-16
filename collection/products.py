f86c2701d4345e4575bc29bd267328a2935cfa13
tiana-horn <tiana.horn@gmail.com>
2018-11-15 22:37:48 -0500

    Items to loop through for products

---
+++ collection/products.py
@@ -0,0 +1,22 @@
+products = [
+    {
+        "imgsrc":
+        "HairAndSkin.jpg",
+        "name":
+        "Cocoa & Shea Butter Hair and Skin Moisturizer (8 oz.)",
+        "description":
+        "Moisturize without compromise! Our combination of natural, simple and pure ingredients will increase the health of your hair without adding any harsh chemicals. Lasts 3-6 months! Ingredients: Shea Butter, Coconut Oil, Cocoa Butter, Grapeseed Oil, Extra Virgin Olive Oil, Sunflower Oil, Rosemary Oil *This product was formerly known as Cocoa and Shea Butter Leave-In Moisturizer. New name, same great product!",
+        "price":
+        $14.99,
+    },
+    {
+        "imgsrc":
+        "HairGel.jpg",
+        "name":
+        "Maximum Hold Baby Hair Gel (2 OZ.)",
+        "description":
+        "Our natural bamboo wooden combs will detangle your hair and now have twice the strength of our wooden hair picks! Great for coarse or thick hair.",
+        "price":
+        $5.97,
+    },
+]
