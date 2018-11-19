d3db76dfefd57a9c75ec5437fe84ac73a214bc50
tiana-horn <tiana.horn@gmail.com>
2018-11-19 00:45:32 -0500

    Product per user

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -7,5 +8,5 @@ class Product(models.Model):
     description = models.TextField()
     pictures = models.ImageField(upload_to='images/', null=True)
     slug = models.SlugField(unique=True)
-    
-
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+    blank=True, null=True)
